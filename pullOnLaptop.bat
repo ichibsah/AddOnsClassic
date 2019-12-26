aed47508f1ab296f010b1492bfd10745ff9cf919
Ibrahim <ibrahim@skyline.lan>
2019-12-26 12:42:57 +0100

    26/12/2019 12:42:57,18

---
+++ pullOnLaptop.bat
@@ -0,0 +1,9 @@
+rem https://github.com/ichibsah/AddOnsClassic/archive/master.zip
+
+c:
+cd Program Files (x86)\World of Warcraft\_classic_\Interface\AddOns
+git clone https://github.com/ichibsah/AddOnsClassic.git c:\Program Files (x86)\World of Warcraft\_classic_\Interface\AddOns
+git remote add origin  https://github.com/ichibsah/AddOnsClassic.git
+git pull https://github.com/ichibsah/AddOnsClassic.git
+
+pause
